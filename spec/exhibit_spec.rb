09699dd5c1c4ea0089c6afd3d1e81ae10189fbcc
Ken Lenhart <omnipaid@gmail.com>
2022-07-27 08:41:24 -0700

    Add exhibit class, patron class, update spec files

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,5 +2,15 @@ require './lib/exhibit'
 require './lib/patron'
 
 RSpec.describe Exhibit do
-  
+  let!(:exhibit) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
+  context 'exhibit' do
+    it 'exists' do 
+      expect(exhibit).to be_a(Exhibit)
+    end
+
+    it 'has details' do
+      expect(exhibit.name).to eq("Gems and Minerals")
+      expect(exhibit.cost).to eq(0)
+    end
+  end
 end
